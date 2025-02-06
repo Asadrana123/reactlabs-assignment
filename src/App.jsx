0852312c742ec5f13567bc20f827ab9d71ad0a28
Asad Rana <asadlukman246@gmail.com>
2025-02-06 12:45:43 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
-import Counter from "./components/counter/Counter";
 import AuthenticationPage from "./Pages/AuthenticationPage";
 import { useSelector } from "react-redux";
 function App() {
