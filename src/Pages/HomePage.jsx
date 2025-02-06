f785a2e75ab85ce58b0dc2c77b77063723e46624
Asad Rana <asadlukman246@gmail.com>
2025-02-06 12:56:21 +0530

    edits

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,7 +3,7 @@ import Counter from '../components/Counter/Counter'
 import TextEditor from '../components/TextEditor/TextEditor'
 import DataForm from '../components/DataForm/DataForm'
 import Additional from '../components/Additional/Additional'
-import "./HomePage.css"
+import "./Homepage.css"
 function HomePage() {
   return (
     <div className='homepage-container'>
